8551742fd1dac9cc9ae66be1e226ead496b5693e
Candace Farris <candace.farris808@gmail.com>
2015-04-30 15:54:22 -1000

    Edited teardown method
    Let me know if I need to add anything else

--- GroupProjectFTP/src/com/groupone/SocketInteractions.java
+++ GroupProjectFTP/src/com/groupone/SocketInteractions.java
@@ -73,8 +73,10 @@ public abstract class SocketInteractions {
         getObjectOutputStream().close();
         getObjectInputStream().close();
         getClientSocket().close();
-
+        System.exit(1); //Terminates currently running program
     }
+    //Allows
+    
 
     /*
     -------------------------------------GETTERS AND SETTERS------------------------------------------
